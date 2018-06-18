153eb67b6fcf72b0d3e6bbafa20370883ccef82b
the mongesan <37490690+mongesan@users.noreply.github.com>
2018-06-18 16:46:41 +0900

    console

--- 3-10-2b.js
+++ 3-10-2b.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded',function(){
     var h = document.getElementById("title");
     h.textContent = "はじめまして";
+    console.log("called: DOMContentLoaded");
 });
+window.onload = function(){
+    console.log("called: window.onload")
+}
