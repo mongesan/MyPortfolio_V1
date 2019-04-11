23625e27e68f787a3be2517614105ed98856b932
the mongesan <37490690+mongesan@users.noreply.github.com>
2019-04-11 16:03:56 +0900

    re_start

--- 3-10-2b.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded',function(){
-    var h = document.getElementById("title");
-    h.textContent = "はじめまして";
-    console.log("called: DOMContentLoaded");
-});
-window.onload = function(){
-    console.log("called: window.onload")
-}
--- README.md
+++
@@ -1,4 +0,0 @@
-# mongesan.github.io
-mongesanのjs
-scratch... @the_newmonge_sc2
-よろしくお願いします
