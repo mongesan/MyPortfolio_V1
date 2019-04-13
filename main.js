763f326e1a12bd0894a00f56253c4d240cb7870f
Haruto Hirakawa <31243896+JPNYKW@users.noreply.github.com>
2019-04-13 15:48:48 +0900

    fix

---
+++ main.js
@@ -0,0 +1,23 @@
+(() => {
+  onload = () => {
+    const coming = document.getElementsByClassName('coming')[0];
+    coming.style.letterSpacing = '3px';
+
+    coming.innerText.split('').map((char, index) => {
+      if (index === 0) coming.innerText = '';
+
+      const span = document.createElement('span');
+      span.innerText = char;
+
+      // アニメーション
+      span.style = `
+        opacity: 0;
+        bottom: 100px;
+        position: relative;
+        animation: Test2 1200ms ${300 + index * 80}ms ease-in-out forwards
+      `;
+
+      coming.appendChild(span);
+    });
+  };
+})();
