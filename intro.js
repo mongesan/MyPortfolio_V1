0859876d5176fb04b16b74c9f12fdaa8c4ca020c
the mongesan <37490690+mongesan@users.noreply.github.com>
2019-04-14 20:42:04 +0900

    Update 0.6
    Ecoders tokyo 19
    Contributed by @jpnykw
    Thanks!

---
+++ intro.js
@@ -0,0 +1,27 @@
+(() => {
+  onload = () => {
+    const coming = document.getElementsByClassName('coming')[0];
+    coming.style.letterSpacing = '3px';
+
+    const text = 'Coming soon...';
+    text.split('').map((char, index) => {
+      const span = document.createElement('span');
+      span.innerText = char;
+
+      // アニメーション
+      span.style = `
+        opacity: 0;
+        bottom: 100px;
+        position: relative;
+        animation: feedin 1200ms ${300 + index * 80}ms ease-in-out forwards;
+      `;
+      coming.appendChild(span);
+    });
+    // アニメーションその2
+    coming.style = `
+        position: relative;
+        animation: feedout 1200ms ${1300 + text.length * 80}ms ease-in-out forwards;
+      `
+    //contribute by @JPNYKW !
+  };
+})();
--- main.js
+++ main.js
@@ -1,27 +1,5 @@
 (() => {
-  onload = () => {
-    const coming = document.getElementsByClassName('coming')[0];
-    coming.style.letterSpacing = '3px';
-
-    const text = 'Coming soon...';
-    text.split('').map((char, index) => {
-      const span = document.createElement('span');
-      span.innerText = char;
-
-      // アニメーション
-      span.style = `
-        opacity: 0;
-        bottom: 100px;
-        position: relative;
-        animation: Test2 1200ms ${300 + index * 80}ms ease-in-out forwards;
-      `;
-      coming.appendChild(span);
-    });
-    // アニメーションその2
-    coming.style = `
-        position: relative;
-        animation: Test3 1200ms ${1300 + text.length * 80}ms ease-in-out forwards;
-      `
-    //contribute by @JPNYKW !
-  };
+    onload = () => {
+        
+    };
 })();
